4c74b7e7732a8f9168b34756893c169f5b17c0fd
Jae Yoon <jyoon2286@gmail.com>
2023-08-18 00:53:59 -0400

    DAG file to test shell script

---
+++ dags/dags_bash_test_shell.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_test_shell",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2023, 8, 1, tz="US/Eastern"),
+    catchup=False
+) as dag:
+    
+    t1_orange = BashOperator(
+        task_id="t1_orange",
+        bash_command="/opt/lear_airflow/plugins/shell/select_fruit.sh ORANGE",
+    )
+
+    t2_avocado = BashOperator(
+        task_id="t2_avocado",
+        bash_command="/opt/lear_airflow/plugins/shell/select_fruit.sh AVOCADO",
+    )
+
+    t1_orange >> t2_avocado
