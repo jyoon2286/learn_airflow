2d1967059e8ce8b278f39041f8a5d17096d95d8b
Jae Yoon <jyoon2286@gmail.com>
2023-08-21 21:34:33 -0400

    testing python import function

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,18 @@
+import datetime
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="US/Eastern"),
+    catchup=False
+) as dag:
+
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
+    )
