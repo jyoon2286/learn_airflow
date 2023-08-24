fa51eb71f31df0cab86693e0d4207450de64a997
Jae Yoon <jyoon2286@gmail.com>
2023-08-24 14:19:04 -0400

    testing xcom for Bash

--- dags/python_bash_xcom.py
+++ dags/python_bash_xcom.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_xcom",
     schedule="10 0 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 8, 1, tz="US/Eastern"),
     catchup=False
 ) as dag:
     bash_push = BashOperator(
