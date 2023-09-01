fe9aea89bf3905fe2b2d24ef493e6f2c3aa662a6
Jae Yoon <jyoon2286@gmail.com>
2023-09-01 17:18:16 -0400

    customized operator to pull data from api.data.gov and save it into a csv file

---
+++ dags/dags_data_gov_college_score.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+import pendulum
+import json
+
+with DAG(
+    dag_id='dags_simple_http_operator',
+    start_date=pendulum.datetime(2023, 8, 1, tz="US/Eastern"),
+    catchup=False,
+    schedule=None
+) as dag:
+    
+    '''College Score Information'''
+    college_score = SimpleHttpOperator(
+        task_id='college_score',
+        http_conn_id='data.gov',
+        endpoint='/ed/collegescorecard/v1/schools?api_key={{var.value.apikey_data_gov}}',
+        method='GET'
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='college_score')
+        import json
+        from pprint import pprint       
+        pprint(json.loads(rslt))
+        
+    college_score >> python_2()
---
+++ plugins/operators/api_data_gov_to_csv_operator.py
@@ -0,0 +1,48 @@
+from airflow.models.baseoperator import BaseOperator
+from airflow.hooks.base import BaseHook
+import pandas as pd 
+
+class DatagovApiToCsvOperator(BaseOperator):
+    template_fields = ('endpoint', 'path','file_name','base_dt')
+
+    def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
+        super().__init__(**kwargs)
+        self.http_conn_id = 'data.gov'
+        self.path = path
+        self.file_name = file_name
+        self.endpoint = dataset_nm + '{{var.value.apikey_data_gov}}' 
+        self.base_dt = base_dt
+
+    def execute(self, context):
+        import os
+        
+        connection = BaseHook.get_connection(self.http_conn_id)
+        self.base_url = f'http://{connection.host}/{self.endpoint}'
+        row_df = self._call_api(self.base_url)
+        if not os.path.exists(self.path):
+            os.system(f'mkdir -p {self.path}')
+        row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
+
+    def _call_api(self, base_url):
+        import requests
+        import json 
+
+        headers = {'Content-Type': 'application/json',
+                   'charset': 'utf-8',
+                   'Accept': '*/*'
+                   }
+
+        request_url = f'{base_url}'
+        if self.base_dt is not None:
+            request_url = f'{base_url}/{self.base_dt}'
+        response = requests.get(request_url, headers)
+        contents = json.loads(response.text)
+
+        emp_dict={}
+        for result in range(0,len(contents["results"])):
+            school_name = contents["results"][result]["school"]["name"]
+            emp_dict[school_name] = contents["results"][result]["school"]
+        school_df = pd.DataFrame.from_dict(emp_dict, orient='index')
+    
+
+        return school_df
